97654b16d9962f704c12ff4be507ab9749e2bd35
Tate Stone <tatejamesstone@gmail.com>
2019-08-21 09:49:05 -0600

    Finished mouse party

--- vanilla-mouse-party/index.js
+++ vanilla-mouse-party/index.js
@@ -1,32 +1,48 @@
 const mousePosition = {x:0, y:0}
 const drawId = []
 function getRandomNumber(min, max) {
-    return Math.round(Math.random() * (max - min + 1)) + min;
+    return Math.round(Math.random() * (max - min + 1)) + min
   };
   window.addEventListener('mousemove', function(mouseMoveEvent){
-      mousePosition.x = mouseMoveEvent.pageX;
-      mousePosition.y = mouseMoveEvent.pageY;
+      mousePosition.x = mouseMoveEvent.pageX
+      mousePosition.y = mouseMoveEvent.pageY
   })
   function draw (){
-      return setInterval(cb, 1000)
-  }
-  let contatiner = document.getElementById('wrap')
-  
-  
-  const color = `background:rgb(${getRandomNumber(
-    0,
-    255
-  )},${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)});`;
-  const ballSize = getRandomNumber(10, 30);
-  const size = `height:${ballSize}px; width:${ballSize}px;`;
-  const left = `left:${getRandomNumber(
-    mousePosition.x - ballSize,
-    mousePosition.x
-  )}px;`;
-  const top = `top:${getRandomNumber(
-    mousePosition.y - ballSize,
-    mousePosition.y
-  )}px; `;
-  const style = `${left}${top}${color}${size}`;
+      return setInterval(function (){
+        let contatiner = document.getElementById('wrap')
+        const color = `background:rgb(${getRandomNumber(
+            0,
+            255
+          )},${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)});`
+          const ballSize = getRandomNumber(10, 30);
+          const size = `height:${ballSize}px; width:${ballSize}px;`
+          const left = `left:${getRandomNumber(
+            mousePosition.x - ballSize,
+            mousePosition.x
+          )}px;`;
+          const top = `top:${getRandomNumber(
+            mousePosition.y - ballSize,
+            mousePosition.y
+          )}px; `;
+          const style = `${left}${top}${color}${size}`
+        
+          let ball = document.createElement('div')
+             ball.classList.add('ball')
+             ball.style = style
+          
 
-  let ball = document.getElementById("ball")
+        ball.addEventListener('animationend', function(mouseMoveEvent){
+            mouseMoveEvent.target.remove()
+        })
+        contatiner.appendChild(ball)
+      }, 1000) }
+
+    window.addEventListener('mouseover', function(){
+        drawId = draw() 
+    })
+    window.addEventListener('mouseout', function(){
+        clearInterval(drawId)
+    })
+  
+  
+  
